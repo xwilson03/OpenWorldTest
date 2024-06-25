db53e0e8eac7b89c6548c53a81cf95d4f1834681
Xander Wilson <xanderwilson2014@gmail.com>
2024-06-25 17:41:25 -0500

    adds static types, fixes whitespace

--- scripts/input_controller.gd
+++ scripts/input_controller.gd
@@ -6,17 +6,19 @@ var camera_x: float = 0
 var camera_y: float = 0
 
 
-func _init():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+func _init() -> void:
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _input(event):
-	# Handle camera rotation via mouse movement
-	if event is InputEventMouseMotion:
-		# Update cumulative rotation, but clamp vertical values
-		camera_x += -event.relative.x * Globals.X_SENSITIVITY
-		camera_y += -event.relative.y * Globals.Y_SENSITIVITY
-		camera_y = clamp(camera_y, Globals.Y_ROT_MIN, Globals.Y_ROT_MAX)
-		
-		# Emit camera values
-		Globals.set_camera_x.emit(camera_x)
-		Globals.set_camera_y.emit(camera_y)
+func _input(event: InputEvent) -> void:
+    # Handle camera rotation via mouse movement
+    if event is InputEventMouseMotion:
+        var mouse_event := event as InputEventMouseMotion
+        
+        # Update cumulative rotation, but clamp vertical values
+        camera_x += -mouse_event.relative.x * Globals.X_SENSITIVITY
+        camera_y += -mouse_event.relative.y * Globals.Y_SENSITIVITY
+        camera_y = clamp(camera_y, Globals.Y_ROT_MIN, Globals.Y_ROT_MAX)
+        
+        # Emit camera values
+        Globals.set_camera_x.emit(camera_x)
+        Globals.set_camera_y.emit(camera_y)
