0f21e3a1f5e1ec34e362ba05837085388f48bbce
Xander Wilson <xanderwilson2014@gmail.com>
2024-06-25 18:29:51 -0500

    updates camera_controller types, whitespace

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -8,36 +8,36 @@ class_name CameraController
 
 var camera_spring: SpringArm3D
 
-func _input(_event):
-	if Input.is_action_just_pressed("zoom_in"):
-		_on_zoom_in()
-	elif Input.is_action_just_pressed("zoom_out"):
-		_on_zoom_out()
+func _input(_event: InputEvent) -> void:
+    if Input.is_action_just_pressed("zoom_in"):
+        _on_zoom_in()
+    elif Input.is_action_just_pressed("zoom_out"):
+        _on_zoom_out()
 
-func _ready():
-	Globals.set_camera_y.connect(_on_set_camera_y)
-	
-	camera_spring = get_child(0)
-	if camera_spring == null:
-		print("Player: Unable to find Camera Spring node.")
+func _ready() -> void:
+    Globals.set_camera_y.connect(_on_set_camera_y)
+    
+    camera_spring = get_child(0)
+    if camera_spring == null:
+        print("Player: Unable to find Camera Spring node.")
 
-func _on_set_camera_y(value: float):
-	# Reset and update camera y rotation to prevent precision loss
-	transform.basis = Basis()
-	rotate_x(value)
+func _on_set_camera_y(value: float) -> void:
+    # Reset and update camera y rotation to prevent precision loss
+    transform.basis = Basis()
+    rotate_x(value)
 
-func _on_zoom_in():
-	var new_length = clamp(
-		camera_spring.spring_length - distance_per_scroll,
-		min_follow_distance,
-		max_follow_distance
-	)
-	camera_spring.spring_length = new_length
-	
-func _on_zoom_out():
-	var new_length = clamp(
-		camera_spring.spring_length + distance_per_scroll,
-		min_follow_distance,
-		max_follow_distance
-	)
-	camera_spring.spring_length = new_length
+func _on_zoom_in() -> void:
+    var new_length: float = clamp(
+        camera_spring.spring_length - distance_per_scroll,
+        min_follow_distance,
+        max_follow_distance
+    )
+    camera_spring.spring_length = new_length
+    
+func _on_zoom_out() -> void:
+    var new_length: float = clamp(
+        camera_spring.spring_length + distance_per_scroll,
+        min_follow_distance,
+        max_follow_distance
+    )
+    camera_spring.spring_length = new_length
