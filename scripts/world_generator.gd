507f9c087f1dbdaa862b590776e6b9c4b3327e33
Xander Wilson <xanderwilson2014@gmail.com>
2024-06-24 21:30:01 -0500

    adds chunk signal support to world_generator

--- scripts/world_generator.gd
+++ scripts/world_generator.gd
@@ -14,6 +14,9 @@ func _init():
     Globals.world_radius = world_radius
     Globals.chunk_size = chunk_size
     
+    # Connect global signals
+    Globals.entered_new_chunk.connect(_on_entered_new_chunk)
+    
     # Allocate chunk array
     var row_size: int = ((Globals.world_radius * 2) - 1)
     world_chunks.resize(row_size ** 2)
@@ -32,3 +35,14 @@ func _init():
         
         world_chunks.insert(i, new_chunk)
         add_child(new_chunk)
+
+func _on_entered_new_chunk(direction):
+    match direction:
+        Globals.DIRECTION.X_POS:
+            print("X+")
+        Globals.DIRECTION.X_NEG:
+            print("X-")
+        Globals.DIRECTION.Z_POS:
+            print("Z+")
+        Globals.DIRECTION.Z_NEG:
+            print("Z-")
