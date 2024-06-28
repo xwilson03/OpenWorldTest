1837877a8b68b129c45dcabdb25badd095921abb
Xander Wilson <xanderwilson2014@gmail.com>
2024-06-28 07:35:44 -0500

    adds lod control script to chunks

---
+++ scripts/chunk.gd
@@ -0,0 +1,33 @@
+extends Node
+class_name Chunk
+
+@export var high_lod: Node3D
+@export var med_lod: Node3D
+@export var low_lod: Node3D
+
+enum LOD {
+    HIGH,
+    MEDIUM,
+    LOW,
+}
+
+func _init() -> void:
+    set_lod(LOD.LOW)
+
+func set_lod(lod: LOD) -> void:
+    match lod:
+
+        LOD.HIGH:
+            remove_child(low_lod)
+            remove_child(med_lod)
+            add_child(high_lod)
+
+        LOD.MEDIUM:
+            remove_child(low_lod)
+            add_child(med_lod)
+            remove_child(high_lod)
+
+        LOD.LOW:
+            add_child(low_lod)
+            remove_child(med_lod)
+            remove_child(high_lod)
