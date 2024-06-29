99272b408f907a2881ffb6f4c4e51ca4d6b62c68
Xander Wilson <xanderwilson2014@gmail.com>
2024-06-29 06:33:38 -0500

    starts player_xz callback in chunk_grid

--- scripts/chunk_grid.gd
+++ scripts/chunk_grid.gd
@@ -103,7 +103,23 @@ func _delete_row(y: int) -> void:
         _delete_chunk(i, y)
 
 func _on_player_xz(pos: Vector2) -> void:
-    pass
+    var center := Vector2(
+        ((_bounds[RIGHT] - _bounds[LEFT] + 1) / 2.0) * _chunk_size.x + _offset.x,
+        ((_bounds[BOTTOM] - _bounds[TOP] + 1) / 2.0) * _chunk_size.y + _offset.y
+    )
+
+    print(_offset)
+    print(pos, "\t", center)
+
+    #if ((pos.x - center.x) > _chunk_size.x):
+        #_move(Globals.DIRECTION.X_POS)
+    #elif ((pos.x - center.x) < -_chunk_size.x):
+        #_move(Globals.DIRECTION.X_NEG)
+#
+    #if ((pos.y - center.y) > _chunk_size.y):
+        #_move(Globals.DIRECTION.Z_POS)
+    #elif ((pos.y - center.y) < -_chunk_size.y):
+        #_move(Globals.DIRECTION.Z_NEG)
 
 func _move(direction: Globals.DIRECTION) -> void:
     match direction:
