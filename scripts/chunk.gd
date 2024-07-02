0b59ae9fd53d3c2525833f97e16ae95f1e1f236b
Xander Wilson <xanderwilson2014@gmail.com>
2024-07-02 04:50:25 -0500

    fixes and optimizes chunk script

--- scripts/chunk.gd
+++ scripts/chunk.gd
@@ -3,7 +3,7 @@ class_name Chunk
 
 @export var _lod: Globals.LOD
 @export var _this: Node3D
-@export var _children: Array[Node3D]
+@export var _children: Array[Chunk]
 
 var _showing_children: bool
 
@@ -12,22 +12,29 @@ func _ready() -> void:
         remove_child(chunk)
     _showing_children = false
 
-    Globals.player_xz.connect(_on_player_xz)
+    if (Globals.lod_ranges[_lod] > 0):
+        Globals.player_xz.connect(_on_player_xz)
 
 func _on_player_xz(pos: Vector2) -> void:
     var dist_to_player: float = (pos - Vector2(position.x, position.z)).length()
-    if dist_to_player <= Globals.lod_ranges[_lod] and not _showing_children:
+    if dist_to_player <= Globals.lod_ranges[_lod]:
         _show_children()
     else:
         _show_self()
 
 func _show_children() -> void:
+    if _showing_children:
+        return
+
     remove_child(_this)
     for child in _children:
         add_child(child)
     _showing_children = true
 
 func _show_self() -> void:
+    if not _showing_children:
+        return
+
     for child in _children:
         remove_child(child)
     add_child(_this)
